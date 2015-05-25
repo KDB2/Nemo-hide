15e3c01dc4e08a9b221c411384d638d461d09771
KillerdeBug <KillerdeBug@server.fake>
2015-05-25 12:11:43 +0200

    .deb packaging

--- generate-pot-file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This script generates the po/nemo-hide.pot file
-FILEPATH="$(readlink -f "$0")"
-DIR="$(dirname "$FILEPATH")"
-cd "$DIR"
-xgettext --package-name=nemo-hide --package-version=1 -cTRANSLATORS \
-         "extension/nemo-hide.py" -o "po/nemo-hide.pot"
